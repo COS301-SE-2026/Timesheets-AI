b178157e55536ee27ce0c74d205db407131d8729
unknown <u22783386@tuks.co.za>
2026-05-15 18:35:10 +0200

    feat: creating the TimeEntry dto
    this will map what the frontend should send

--- apps/backend/src/main/java/timesheets/dto/request/TimeEntryRequest.java
+++ apps/backend/src/main/java/timesheets/dto/request/TimeEntryRequest.java
@@ -1,5 +1,25 @@
-package com.timesheets.dto.request;
+package timesheets.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDateTime;
+
+//DTO - data transfer object
+
+// this file will essentially be what the front-end sends to create the time-entry
+// defines the JSON that frontend must send and then SProngBoot will automatically map the JSON to the object field getters and setters
 public class TimeEntryRequest {
+
+    @NotNull(message = "Project ID is required")
+    private Long projectId;
+
+    private Long taskId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @Positive(message = "Duration must be positive")
+    private Integer durationMinutes;
+    private String entryType;
+    private String description;
     
 }
