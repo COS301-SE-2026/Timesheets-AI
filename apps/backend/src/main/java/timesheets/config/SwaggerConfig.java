8a2a805274033c6a710e2d28ffe805239e1f7534
unknown <u22783386@tuks.co.za>
2026-05-15 16:44:45 +0200

    chore: updated swagger config file

--- apps/backend/src/main/java/timesheets/config/SwaggerConfig.java
+++ apps/backend/src/main/java/timesheets/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-//just to customize the title and description on how the document looks
+//just to customize the title and description on how the document looks.
 @Configuration
 public class SwaggerConfig {
 
