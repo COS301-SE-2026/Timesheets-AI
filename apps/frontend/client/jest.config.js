533d2fde4732a729c91fbfa81bff93bbb373fa63
LeratoNS <sibandalerato233@gmail.com>
2026-05-16 21:55:42 +0200

    fix: update git configuration instruction

--- apps/frontend/client/jest.config.js
+++ apps/frontend/client/jest.config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
+  testEnvironment: 'jsdom',
 };
