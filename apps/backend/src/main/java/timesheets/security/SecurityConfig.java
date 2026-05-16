fbbee8a7696181aa80e7a0fb0abd236f7786aa68
unknown <u22783386@tuks.co.za>
2026-05-16 23:15:11 +0200

    feat: added a blank SecurityConfig file

--- apps/backend/src/main/java/timesheets/security/SecurityConfig.java
+++ apps/backend/src/main/java/timesheets/security/SecurityConfig.java
@@ -1,5 +1,5 @@
-package com.timesheets.security;
+package timesheets.security;
 
 public class SecurityConfig {
-    
+    //I will implement this, I just need to research on this a bit, unless someone else did it
 }
