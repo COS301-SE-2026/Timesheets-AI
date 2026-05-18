52b9c5fd354efc0f040d179da38adad2a8b9386e
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-05-19 01:02:11 +0200

    Fix(frontend): resolve login test environment and clean dependency lockfiles

--- apps/frontend/client/src/app/features/login/login.component.spec.ts
+++ apps/frontend/client/src/app/features/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +9,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      imports: [
+        LoginComponent,
+        RouterTestingModule
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
