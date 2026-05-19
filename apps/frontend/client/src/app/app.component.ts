0ed3e2bf2a37b8d4b3c20553db10628e29e98db6
kc-node <kc-node1@gmail.com>
2026-05-19 19:13:02 +0200

    feat: create dashboard structure

--- apps/frontend/client/src/app/app.component.ts
+++ apps/frontend/client/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TimesheetsComponent } from './features/timesheets/timesheets.component';
 import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TimesheetsComponent, SidebarComponent, HeaderComponent],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
