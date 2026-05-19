b76bf424a08c6c51b2dd1050c0258c750aace1f2
kc-node <kc-node1@gmail.com>
2026-05-19 20:50:10 +0200

    feat(dropdown): add reusable dropdown selector component

---
+++ apps/frontend/client/src/app/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,34 @@
+import { Component, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.scss'
+})
+export class DropdownComponent {
+  //The dropdown receives a list of strings from outside.
+  options = input<string[]>([]);
+  /*
+  dropdown starts CLOSED (false)
+  becomes true when opened
+  */
+  isOpen = signal(false);
+
+  //What the user selected
+  selectedOption = signal('');
+
+  // read the current state and flips it 
+  toggleDropDown(){
+    this.isOpen.update(open => !open);
+  }
+
+  // closes menu after value is set
+  selectOption(option: string){
+    this.selectedOption.set(option);
+    this.isOpen.set(false);
+
+  }
+}
