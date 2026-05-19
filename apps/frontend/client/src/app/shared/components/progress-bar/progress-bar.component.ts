eaba58ab7ca0c670ba98a59abbb0994221b937d4
kc-node <kc-node1@gmail.com>
2026-05-19 21:15:12 +0200

    feat(progress): add reusable progress bar component

---
+++ apps/frontend/client/src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  standalone: true,
+  templateUrl: './progress-bar.component.html',
+  styleUrl: './progress-bar.component.scss'
+})
+export class ProgressBarComponent {
+  // if the parent does not pass a value, it will starts at 0%
+  progress = input<number>(0);
+}
