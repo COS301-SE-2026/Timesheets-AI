e1d04ad31fa058d16079003d8ccf5c110cbf8f0d
kc-node <kc-node1@gmail.com>
2026-05-19 21:24:04 +0200

    feat(router): add routes configuration with default redirect to timesheets

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TimesheetsComponent } from './features/timesheets/timesheets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'timesheets', pathMatch:'full'},
+    { path: 'timesheets', component: TimesheetsComponent}
+];
