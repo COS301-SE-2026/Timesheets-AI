42afe07eb82f520e3d7e4ae7f72eca3eeddb6216
kc-node <kc-node1@gmail.com>
2026-05-20 21:50:32 +0200

    fix: the timesheets path and syntax error

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -5,10 +5,10 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./features/signup/signup/signup.component').then((m) => m.SignupComponent)
-  }
+  },
   
   {
     path: 'timesheets',
-    loadComponent: () => import('./features/timesheets/timesheets/timesheets.component').then((m) => m.TimesheetsComponent)
+    loadComponent: () => import('./features/timesheets/timesheets.component').then((m) => m.TimesheetsComponent)
   }
 ];
