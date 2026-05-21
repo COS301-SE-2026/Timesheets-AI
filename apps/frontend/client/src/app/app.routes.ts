31b9fb74cbe5901f5ca225c6c1fe612e7d930f6f
LeratoNS <sibandalerato233@gmail.com>
2026-05-21 04:02:40 +0200

    feat: add logtime page to routing file

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'log-time',
+    pathMatch: 'full'
+  },
+  {
+    path: 'log-time',
+    loadComponent: () =>
+      import('./features/users/developer/logtime/logtime.component').then(
+        (m) => m.LogtimeComponent
+      )
+  }
+];
