bf09730aa4944ee439d2ca62e5338846d4195220
kc-node <kc-node1@gmail.com>
2026-05-22 01:26:27 +0200

    fix: remove ButtonComponent in app.component.ts, removed footer in app.component.html and add title line for header.component

--- apps/frontend/client/src/app/app.component.ts
+++ apps/frontend/client/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
