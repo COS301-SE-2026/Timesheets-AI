5c8e27b132eb2921b01ec7d5b8c2fba289701c7e
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-05-22 02:19:48 +0200

    test(frontend): fix NotFoundComponent unit test to use Location service

--- apps/frontend/client/src/app/pages/not-found/not-found.component.spec.ts
+++ apps/frontend/client/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,23 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundComponent]
-    })
-    .compileComponents();
+      imports: [NotFoundComponent],
+      providers: [
+        {
+          provide: Location,
+          useValue: {
+            back: jest.fn(),
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {}, //fixes injector crash
+        },
+      ],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(NotFoundComponent);
+    const fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call location.back when goBack is triggered', () => {
+    const location = TestBed.inject(Location);
+
+    component.goBack();
+
+    expect(location.back).toHaveBeenCalled();
   });
 });
