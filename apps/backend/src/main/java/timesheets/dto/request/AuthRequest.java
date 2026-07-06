70c75e1448dbe3bbf4916042d2476b370961cba4
unknown <u22783386@tuks.co.za>
2026-07-06 17:01:39 +0200

    chore: uncommented the AuthRequest
    towards implementing the SSO, we are going to have the GoogleAuth request as well as this one, so this is going to be needed.

--- apps/backend/src/main/java/timesheets/dto/request/AuthRequest.java
+++ apps/backend/src/main/java/timesheets/dto/request/AuthRequest.java
@@ -1,22 +1,22 @@
-// package timesheets.dto.request;
+package timesheets.dto.request;
 
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-// import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
-// // a simple request object for login, which only contains email and password, and an optional
-// // rememberMe field for 30-day session.
+// a simple request object for login, which only contains email and password, and an optional
+// rememberMe field for 30-day session.
 
-// @Data
-// public class AuthRequest {
+@Data
+public class AuthRequest {
 
-//   @NotBlank(message = "email is required")
-//   @Email(message = "invalid email format")
-//   private String email;
+  @NotBlank(message = "email is required")
+  @Email(message = "invalid email format")
+  private String email;
 
-//   @NotBlank(message = "password is required")
-//   private String password;
+  @NotBlank(message = "password is required")
+  private String password;
 
-//   // commented this out for now, we dont have this in the db.
-//   // private Boolean rememberMe; // for 30-day session
-// }
+  // commented this out for now, we dont have this in the db.
+  // private Boolean rememberMe; // for 30-day session
+}
