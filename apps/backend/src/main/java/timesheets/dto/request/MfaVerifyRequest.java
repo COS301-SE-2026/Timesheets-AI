b5afa928b799d805f5a7e8b1051471bd9528aef5
unknown <u22783386@tuks.co.za>
2026-07-06 20:51:48 +0200

    feat: created the Mfa request verification DTO

---
+++ apps/backend/src/main/java/timesheets/dto/request/MfaVerifyRequest.java
@@ -0,0 +1,12 @@
+package timesheets.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+//so Time-Based One-Time Password is a standard for authenticator apps
+//for now the authenticator app we are using is google
+
+public class MfaVerifyRequest {
+    @NotBlank(message = "TOTP code is required")
+    private String totpCode; 
+}
