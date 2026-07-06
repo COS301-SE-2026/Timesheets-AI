ff02877965f5d6a1ec5b5c484a58cc8b90ee10aa
unknown <u22783386@tuks.co.za>
2026-07-06 18:29:25 +0200

    feat: created the MFA setup response

---
+++ apps/backend/src/main/java/timesheets/dto/response/MfaSetupResponse.java
@@ -0,0 +1,17 @@
+package timesheets.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//conatins the info for MFA setup, that is returned after 2FA is initiated
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MfaSetupResponse {
+    private String secretKey; //this is what the authenticator app uses to generate the TOTP
+    private String qrCodeUrl; //I want this to contain a link to generating URL's with authenticator apps
+    private String message;
+}
