5ee61993e708f1884af402a46d7205337fbde239
unknown <u22783386@tuks.co.za>
2026-07-06 17:05:49 +0200

    feat: created the GoogleAuth request

---
+++ apps/backend/src/main/java/timesheets/dto/response/GoogleUserInfoRespose.java
@@ -0,0 +1,10 @@
+package timesheets.dto.response;
+
+import lombok.Data;
+
+@Data
+public class GoogleUserInfoRespose {
+    private String googleUserId;
+    private String email;
+    private String
+}
