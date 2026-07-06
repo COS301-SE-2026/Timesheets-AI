891e8cd769eec996f4938ccf22fad685eb35d570
unknown <u22783386@tuks.co.za>
2026-07-06 23:45:03 +0200

    chore: refactor the RegisterRequest  dto
    added a regex that is commonly used for password validation and made the messages look better

--- apps/backend/src/main/java/timesheets/dto/request/RegisterRequest.java
+++ apps/backend/src/main/java/timesheets/dto/request/RegisterRequest.java
@@ -1,34 +1,34 @@
-// package timesheets.dto.request;
+package timesheets.dto.request;
 
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-// import jakarta.validation.constraints.Pattern;
-// import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-// // a simple request object for registration, which contains first name, last name, email, and
-// // password.
-// // we will validate the email format and password strength using annotations.
-// @Data
-// public class RegisterRequest {
+// a simple request object for registration, which contains first name, last name, email, and
+// password.
+// we will validate the email format and password strength using annotations.
+@Data
+public class RegisterRequest {
 
-//   @NotBlank(message = "first name is required")
-//   private String firstName;
+  @NotBlank(message = "First name is required")
+  private String firstName;
 
-//   @NotBlank(message = "last name is required")
-//   private String lastName;
+  @NotBlank(message = "Last name is required")
+  private String lastName;
 
-//   @NotBlank(message = "email is required")
-//   @Email(message = "invalid email format") // must be .momentum.co.za email
-//   private String email;
+  @NotBlank(message = "Email is required")
+  @Email(message = "Invalid email format") // must be .momentum.co.za email
+  private String email;
 
-//   @NotBlank(message = "password is required")
-//   @Pattern(
-//       regexp =
-//           "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$", // at least 8 characters, one
-// uppercase,
-//       // one number, and one special character
-//       message =
-//           "password must be at least 8 characters with one uppercase, one number, and one special
-// character")
-//   private String password;
-// }
+  @NotBlank(message = "Password is required")
+  @Size(min = 8, message = "Password must be atleast 8 characters")
+  @Pattern(
+      regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$",
+      message =
+          "Password must contain atleast one uppercase letter, one number, and one special character (!@#$%^&*)")
+  private String password;
+}
+
+// password regex from: https://www.baeldung.com/java-regex-password-validation 
