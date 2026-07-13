020f325e377ded24cf9a0b1e32be382c40c0fc6d
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-13 22:50:23 +0200

    feat: created the folder structure for the documentation site following LIFT.

---
+++ documentation-site/src/app/shared/components/document-card/document-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './document-card.component.html',
+  styleUrl: './document-card.component.scss'
+})
+export class DocumentCardComponent {
+
+}
---
+++ documentation-site/src/app/shared/components/folder-card/folder-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-folder-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './folder-card.component.html',
+  styleUrl: './folder-card.component.scss'
+})
+export class FolderCardComponent {
+
+}
