857a0e7b5580397e3c15b25e87f8cda81203cd83
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-13 23:07:27 +0200

    feat: set up the routing pahs for the MVP documentation website.

--- documentation-site/src/app/app.routes.ts
+++ documentation-site/src/app/app.routes.ts
@@ -1,3 +1,29 @@
+/**
+ * Author: Cleopatra Kwenda
+ * Date: 2026-07-13
+ * Purpose: This file defines the routes for the Documentation website and is the central routing place. 
+ * Related Requirement: N/A yet
+ **/
 import { Routes } from '@angular/router';
+import {HomeComponent} from './features/home/home.component';
+import {AboutComponent} from './features/about/about.component';
+import { DocumentationComponent } from './features/documentation/documentation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'documentation',
+        component: DocumentationComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
