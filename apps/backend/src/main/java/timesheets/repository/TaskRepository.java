dcbdd77784fdb87adec4fd895c864589e7c33bc5
unknown <u22783386@tuks.co.za>
2026-07-14 16:39:43 +0200

    uncommented out the repository code
    - just uncommenting out the code that was previously existing for these repositories

--- apps/backend/src/main/java/timesheets/repository/TaskRepository.java
+++ apps/backend/src/main/java/timesheets/repository/TaskRepository.java
@@ -1,16 +1,14 @@
-// package timesheets.repository;
+package timesheets.repository;
 
-// import java.util.List;
-// import java.util.UUID;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import timesheets.domain.Task;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import timesheets.domain.Task;
 
-// @Repository
-// public interface TaskRepository extends JpaRepository<Task, UUID> {
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 
-//   // all the tasks belonging to a specific project (found by ID), will be gotten and stored in
-// the
-//   // list
-//   List<Task> findByProjectId(UUID projectId);
-// }
+  /*all the tasks belonging to a specific project (found by ID), will be gotten and stored in the list*/
+  List<Task> findByProjectId(UUID projectId);
+}
