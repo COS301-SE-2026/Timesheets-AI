f2270c57a96ce7a938e38fbffb8c4f90dc33f134
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-16 00:48:24 +0200

    style: just fixing up the formatting such i can create a PR.

--- apps/frontend/client/src/app/shared/components/hourglass/hourglass.component.ts
+++ apps/frontend/client/src/app/shared/components/hourglass/hourglass.component.ts
@@ -1,9 +1,9 @@
-/** 
+/**
  * Author: Cleopatra Kwenda
  * Date: 2024-07-15
- * Purpose: This component displays the animated hourglass found on the 
+ * Purpose: This component displays the animated hourglass found on the
  * 404 not found page
- * Related Requirement:
+ * Related Requirement: N/A
  */
 import { Component } from '@angular/core';
 
@@ -12,8 +12,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hourglass.component.html',
-  styleUrl: './hourglass.component.scss'
+  styleUrl: './hourglass.component.scss',
 })
-export class HourglassComponent {
-
-}
+export class HourglassComponent {}
