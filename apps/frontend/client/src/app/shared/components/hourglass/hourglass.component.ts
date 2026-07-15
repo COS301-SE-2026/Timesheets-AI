c373beeaef798f4d99d207ec224ff83355c66d84
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-15 14:50:25 +0200

    refactor(hourglass): converted the componenet such it becomes a standalone componenet.

--- apps/frontend/client/src/app/shared/components/hourglass/hourglass.component.ts
+++ apps/frontend/client/src/app/shared/components/hourglass/hourglass.component.ts
@@ -1,7 +1,15 @@
+/** 
+ * Author: Cleopatra Kwenda
+ * Date: 2024-07-15
+ * Purpose: This component displays the animated hourglass found on the 
+ * 404 not found page
+ * Related Requirement:
+ */
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hourglass',
+  standalone: true,
   imports: [],
   templateUrl: './hourglass.component.html',
   styleUrl: './hourglass.component.scss'
