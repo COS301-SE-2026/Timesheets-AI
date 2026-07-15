ca8139e69a4f80e40d9aa70bc39c4f6791a44418
cybersurfe <zwanezamokuhle55@gmail.com>
2026-07-15 18:45:55 +0200

    fix: pipeline was still failing tests, so i had to a the hardcoded faux value.

--- ai-service/app/config.py
+++ ai-service/app/config.py
@@ -13,7 +13,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     # As suggested i moved the placeholder db url and moved it to .env.example
-    database_url: str = ""
+    database_url: str = "postgresql://postgres:postgres@localhost:5432/momently_dev"
 
     # here i'll place the api key for whatever api we use for the ai service. As it stands we may use anthropic's api.
     anthropic_api_key: str = ""  # TODO: Replace with actual API key
