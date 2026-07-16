afe901c5016400860795f376b412610d4c3551c0
unknown <u22783386@tuks.co.za>
2026-07-16 16:05:37 +0200

    feat: added Custom User Details
    - this should help prevent needing to query the DB each time when needing a users details with each request

---
+++ apps/backend/src/main/java/timesheets/security/CustomUserDetails.java
@@ -0,0 +1,67 @@
+package timesheets.security;
+
+import java.util.Collection;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+/*
+- so I are trying to prevent needing DB queries for user details on every request
+- creating a class that will stores additional info beyond what Spring User has
+*/
+@Getter
+public class CustomUserDetails extends User {
+  private final UUID userId;
+  private final String email;
+  private final String firstName;
+  private final String lastName;
+  private final Boolean emailVerified;
+  private final UUID defaultWorkspaceMemberId;
+  private final Collection<GrantedAuthority> authorities;
+
+  public CustomUserDetails(
+      UUID userId,
+      String email,
+      String firstName,
+      String lastName,
+      Boolean emailVerified,
+      UUID defaultWorkspaceMemberId,
+      String passwordHash,
+      boolean enabled,
+      boolean accountNonExpired,
+      boolean credentialsNonExpired,
+      boolean accountNonLocked,
+      Collection<GrantedAuthority> authorities) {
+
+    // the email is the username
+    super(
+        email,
+        passwordHash,
+        enabled,
+        accountNonExpired,
+        credentialsNonExpired,
+        accountNonExpired,
+        authorities);
+
+    this.userId = userId;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailVerified = emailVerified;
+    this.defaultWorkspaceMemberId = defaultWorkspaceMemberId;
+    this.authorities = authorities;
+  }
+
+  // get the full name
+  public String getFullName() {
+    return firstName + " " + lastName;
+  }
+
+  // checks if the user has a specific role
+  public boolean hasRole(String role) {
+    if (authorities == null) return false;
+
+    return authorities.stream().anyMatch(authority -> authority.getAuthority().equals(role));
+  }
+}
