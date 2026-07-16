e7a1811b834866e3f68fbb1ce80f09c8ed27ee51
unknown <u22783386@tuks.co.za>
2026-07-16 22:26:11 +0200

    feat: added SecurityUtils
    - created to get user info from the security context
    - so that there are no constant DB queries

---
+++ apps/backend/src/main/java/timesheets/security/SecurityUtils.java
@@ -0,0 +1,31 @@
+package timesheets.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+/*
+- this is a helper class to get the logged-in user info from the security context
+- there won't be any DB queries since it will be stored in memory
+*/
+@Component
+@RequiredArgsConstructor
+public class SecurityUtils {
+
+  // gets the authenticated user details
+  public CustomUserDetails getCurrentUser() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication == null || !authentication.isAuthenticated()) {
+      throw new RuntimeException("user not authenticated");
+    }
+
+    Object principal = authentication.getPrincipal();
+    if (principal instanceof CustomUserDetails) {
+      return (CustomUserDetails) principal;
+    }
+
+    throw new RuntimeException("Invalid authentication principal. Expected CustomUserDetails");
+  }
+}
