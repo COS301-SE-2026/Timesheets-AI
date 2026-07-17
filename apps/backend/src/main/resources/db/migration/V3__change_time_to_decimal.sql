88812d5913d85a266528239e6c9faec34b867c19
Nyashadzashe M. <138135522+TheDzashe@users.noreply.github.com>
2026-07-17 18:38:48 +0200

    Delete apps/backend/src/main/resources/db/migration/V3__change_time_to_decimal.sql
    Signed-off-by: Nyashadzashe M. <138135522+TheDzashe@users.noreply.github.com>

--- apps/backend/src/main/resources/db/migration/V3__change_time_to_decimal.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE time_entries ADD COLUMN duration_seconds INTEGER;
-UPDATE time_entries SET duration_seconds = duration_minutes * 60;
-ALTER TABLE time_entries DROP COLUMN duration_minutes;
-ALTER TABLE time_entries RENAME COLUMN duration_seconds TO duration_seconds;
