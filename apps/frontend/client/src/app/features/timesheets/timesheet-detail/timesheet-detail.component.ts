665eddae79fb5eb499fe025416c96a9cde65ddd0
LeratoNS <sibandalerato233@gmail.com>
2026-07-18 00:37:57 +0200

    feat: add timesheet detail components

---
+++ apps/frontend/client/src/app/features/timesheets/timesheet-detail/timesheet-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timesheet-detail',
+  imports: [],
+  templateUrl: './timesheet-detail.component.html',
+  styleUrl: './timesheet-detail.component.scss'
+})
+export class TimesheetDetailComponent {
+
+}
