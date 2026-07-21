77936a551af3a77582c69c978a8960224e78b47d
unknown <u22783386@tuks.co.za>
2026-07-21 21:24:08 +0200

    style: applied spotless

--- apps/backend/src/main/java/timesheets/repository/TaskRepository.java
+++ apps/backend/src/main/java/timesheets/repository/TaskRepository.java
@@ -9,15 +9,16 @@ import timesheets.domain.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 
-  //finds all the tasks belonging to a specific project, including the deleted ones
+  // finds all the tasks belonging to a specific project, including the deleted ones
   List<Task> findByProjectId(UUID projectId);
 
-  //finds all the tasks on a project, that are not deleted - think showing all the tasks on a project board??
+  // finds all the tasks on a project, that are not deleted - think showing all the tasks on a
+  // project board??
   List<Task> findByProjectIdAndIsDeletedFalse(UUID projectId);
 
-  //finds all the tasks for a specific member, and the tasks are active - think showing "my tasks"
+  // finds all the tasks for a specific member, and the tasks are active - think showing "my tasks"
   List<Task> findByAssignedWorkspaceMemberIdAndIsDeletedFalse(UUID workspaceMemberId);
 
-  //this checks if a project has any active tasks
+  // this checks if a project has any active tasks
   boolean existsByProjectIdAndIsDeletedFalse(UUID projectId);
 }
