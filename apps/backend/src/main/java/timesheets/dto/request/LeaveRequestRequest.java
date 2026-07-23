4f04dbd465a8d2caa5c6269d46cb92814e3db835
unknown <u22783386@tuks.co.za>
2026-07-23 20:29:49 +0200

    feat: added the update leave request class in the dto

--- apps/backend/src/main/java/timesheets/dto/request/LeaveRequestRequest.java
+++ apps/backend/src/main/java/timesheets/dto/request/LeaveRequestRequest.java
@@ -2,6 +2,7 @@ package timesheets.dto.request;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -15,7 +16,9 @@ import lombok.NoArgsConstructor;
 - all the leave requests dto's
 - inner classes will handle specific logic
 - it's easier to keep them all organised in one file and preventing cluttering the dto folder */
-public @interface LeaveRequestRequest {
+public class LeaveRequestRequest {
+
+  // creating a new leave request
   @Data
   @Builder
   @NoArgsConstructor
@@ -33,6 +36,36 @@ public @interface LeaveRequestRequest {
     private LocalDate startDate;
 
     @NotNull(message = "End date is required")
+    @Future(message = "End date must be in future")
+    private LocalDate endDate;
+
+    @Positive(message = "Total days must be positive")
+    private Double totalDays;
+
+    private String reason;
+
+    // this is going to be a JSON string of attachements
+    private String attachments;
+  }
+
+  /*
+  - request to update a leave request
+  - only pending requests should be updated
+  - rejected or approved requests should not be updatable */
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Update {
+    @Pattern(
+        regexp = "ANNUAL|SICK|MATERNITY|PATERNITY|FAMILY_RESPONSIBILITY|OTHER",
+        message =
+            "Leave type must be ANNUAL, SICK, MATERNITY, PATERNITY, FAMILY_RESPONSIBILITY or OTHER")
+    private String leaveType;
+
+    @FutureOrPresent(message = "Start date cannot be in the past")
+    private LocalDate startDate;
+
     @Future(message = "End date must be in future")
     private LocalDate endDate;
 
