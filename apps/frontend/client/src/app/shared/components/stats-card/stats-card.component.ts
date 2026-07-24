01175b73965472e515d81a5bd006839017f11330
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-24 22:46:28 +0200

    fix: removing the issue with the project board component in imports.

--- apps/frontend/client/src/app/shared/components/stats-card/stats-card.component.ts
+++ apps/frontend/client/src/app/shared/components/stats-card/stats-card.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 @Component({
   selector: 'app-stats-card',
   standalone: true, 
-  imports: [CommonModule, ProgressBarComponent],
+  imports: [CommonModule],
   templateUrl: './stats-card.component.html',
   styleUrl: './stats-card.component.scss'
 })
