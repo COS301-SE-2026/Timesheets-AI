2e28f42aeed97b8c89f77425cbf45b4a736e79f2
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-25 18:47:41 +0200

    feat:fixed the routing issues with the button and projectcard.on to styling now.

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -64,6 +64,14 @@ export const routes: Routes = [
         .then(m => m.ProjectsComponent)
   },
 
+  /* Project Details Page */
+  {
+    path: 'projects/:id',
+    loadComponent: () =>
+      import('./features/projects/project-details/project-details.component')
+        .then(m => m.ProjectDetailsComponent)
+  },
+
   /* 404 fallback */
   {
     path: 'not-found',
