31c96ea1767ef7a5d4225543bbc956e2203b8ae8
kc-node <kc-node1@gmail.com>
2026-07-29 09:39:31 +0200

    fix: add debug to test in ec2

--- apps/backend/src/main/java/timesheets/service/AuthService.java
+++ apps/backend/src/main/java/timesheets/service/AuthService.java
@@ -172,12 +172,20 @@ public class AuthService {
         userRepository
             .findById(verificationToken.getUserId())
             .orElseThrow(() -> new IllegalArgumentException("user not found"));
-    user.setEmailVerified(true);
-    userRepository.save(user);
+            // DEBUG 
+    System.out.println("User before update:" + user.getEmail());
+    System.out.println("Verified already" + user.getEmailVerified());
 
+
+    user.setEmailVerified(true);
+
+    System.out.println("Verified after" + user.getEmailVerified());
+    
+    // userRepository.save(user);
+    userRepository.saveAndFlush(user);
+    
     return new MessageResponse("Email verified successfully", "/dashboard");
   }
-
   /*
    * I am using no rollback such that there is no rollback for login failures.
    * I want the login attempts to still register in the DB
