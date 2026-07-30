98b325313e4a8611908b9dff29e4eeceb8e74735
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-30 19:25:05 +0200

    feat: added the data the page will use for the tiles it has.

--- apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
+++ apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
@@ -9,11 +9,20 @@ Related Requirement: N/A
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FontAwesomModule } from '@fortawesome/angular-fontaesome'
 import { title } from 'node:process';
+import {
+  faClock,
+  faChartLine,
+  faPuzzlePiece,
+  faShieldHalved
+} from '@fortawesome/free-solid-svg-core'
+import { from } from 'node:stream/iter';
+
 @Component({
   selector: 'app-landing-page',
   standalone:true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FontAwesomModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
@@ -22,8 +31,32 @@ export class LandingPageComponent {
   features=[
     {
       title: 'Effortless Tracking',
-      description: 'Track time in rea'
-    }
+      description: 'Track time in real-time or log it later-quick, simple, and flexible.',
+      bgColor: '#eff6ff',
+      iconColor: '#2563eb',
+      iconPath: faClock
+    },
+    {
+      title: 'Seamless Integrations',
+      description: 'Connect with Jira, GitHub, Outlook Calendar and more tools you already use.',
+      bgColor: '#f0fdf4',
+      iconColor: '#16a34a',
+      iconPath: faPuzzlePiece
+    },
+    {
+      title: 'AI-Powered Insights',
+      description: 'Get intelligent analytics and predictive insights that help you work smarter.',
+      bgColor: '#fff7ed',
+      iconColor: '#ea580c',
+      iconPath: faChartLine
+    },
+    {
+      title: 'Accurate & Trusted Data',
+      description: 'AI detects anomalies and ensures your time data is accurate and reliable.',
+      bgColor: '#faf5ff',
+      iconColor: '#9333ea',
+      iconPath: faShieldHalved
+    },
   ]
 
 }
