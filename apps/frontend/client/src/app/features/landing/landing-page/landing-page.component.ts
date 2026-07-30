c00838bc4f0c945deb1a67204d62884b21ebe888
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-30 13:48:29 +0200

    feat: added the initial work but have to change branch quickly hence this hasty commit.

--- apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
+++ apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
@@ -1,6 +1,15 @@
+/*
+Author: Cleopatra Kwenda
+Date: 2026-07-30
+Purpose: Display all the necessary components using other project models
+with only mock that should be eaisly integrated.
+Related Requirement: N/A
+*/
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { title } from 'node:process';
 @Component({
   selector: 'app-landing-page',
   standalone:true,
@@ -9,5 +18,12 @@ import { RouterLink } from '@angular/router';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  
+  features=[
+    {
+      title: 'Effortless Tracking',
+      description: 'Track time in rea'
+    }
+  ]
 
 }
