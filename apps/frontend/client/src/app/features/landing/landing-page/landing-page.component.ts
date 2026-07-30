49d601a3598b4e1e040968ffc5cc29a5436f12ba
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-30 21:03:50 +0200

    refactor: i was doing nonsense i removed the icon dependency and used what i had used for icons for pats pages so yeah was refactoring the icons

--- apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
+++ apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.ts
@@ -7,18 +7,11 @@ Related Requirement: N/A
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import {
-  faClock,
-  faChartLine,
-  faPuzzlePiece,
-  faShieldHalved
-} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-landing-page',
   standalone:true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
@@ -33,28 +26,28 @@ export class LandingPageComponent {
       description: 'Track time in real-time or log it later-quick, simple, and flexible.',
       bgColor: '#eff6ff',
       iconColor: '#2563eb',
-      iconPath: faClock
+      iconPath: 'fa-solid fa-clock'
     },
     {
       title: 'Seamless Integrations',
       description: 'Connect with Jira, GitHub, Outlook Calendar and more tools you already use.',
       bgColor: '#f0fdf4',
       iconColor: '#16a34a',
-      iconPath: faPuzzlePiece
+      iconPath: 'fa-solid fa-puzzle-piece'
     },
     {
       title: 'AI-Powered Insights',
       description: 'Get intelligent analytics and predictive insights that help you work smarter.',
       bgColor: '#fff7ed',
       iconColor: '#ea580c',
-      iconPath: faChartLine
+      iconPath: 'fa-solid fa-chart-line'
     },
     {
       title: 'Accurate & Trusted Data',
       description: 'AI detects anomalies and ensures your time data is accurate and reliable.',
       bgColor: '#faf5ff',
       iconColor: '#9333ea',
-      iconPath: faShieldHalved
+      iconPath: 'fa-solid fa-shiel-halved'
     },
   ];
 
