0748d6154b523e91963812512f6c929e740909fb
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-31 02:40:23 +0200

    fix: addd test import to the testing coding although i have no thorogh test.

--- apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.spec.ts
+++ apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
@@ -8,7 +8,8 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingPageComponent]
+      imports: [LandingPageComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
