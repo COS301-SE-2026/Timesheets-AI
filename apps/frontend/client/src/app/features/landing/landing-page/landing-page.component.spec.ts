6d068fe764ec4ed61f2ae10f7f1db36e369ad7d6
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-07-31 02:53:14 +0200

    fix: added test.

--- apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.spec.ts
+++ apps/frontend/client/src/app/features/landing/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
+import { log } from 'node:console';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -20,5 +21,47 @@ describe('LandingPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the logo', ()=> {
+    const compiled= fixture.nativeElement;
+
+    const logo= compiled.querySelector('.brand-logo');
+
+    expect(logo).toBeTruthy();
+  });
+
+  it('should display the hero title', ()=> {
+    const compiled= fixture.nativeElement;
+
+    expect(compiled.textContent).toContain('Timesheets');
+  });
+
+  it('should display Login button', ()=> {
+    const compiled= fixture.nativeElement;
+
+    expect(compiled.textContent).toContain('Login');
+  });
+
+  it('should display Sign Up button', ()=> {
+    const compiled= fixture.nativeElement;
+
+    expect(compiled.textContent).toContain('Signup');
+  });
+
+  it('should show 4 feature cards', ()=> {
+    const compiled= fixture.nativeElement;
+
+    const cards=compiled.querySelectorAll('.features-card')
+
+    expect(cards.length).toHaveLength(4);
+  });
+
+  it('should show company logos', ()=> {
+    const compiled= fixture.nativeElement;
+
+    const logos= compiled.querySelectorAll('.logos-container img')
+
+    expect(logos.length).toHaveLength(4);
   });
 });
