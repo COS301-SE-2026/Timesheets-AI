60aca5ccdbe8ddf7215da7d5558c09ecc43f69d7
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-08-04 00:36:04 +0200

    test: add the initial dropdown test.

--- apps/frontend/client/src/app/shared/components/dropdown/dropdown.component.spec.ts
+++ apps/frontend/client/src/app/shared/components/dropdown/dropdown.component.spec.ts
@@ -1,4 +1,17 @@
 import { DropdownComponent } from './dropdown.component';
 import { createComponentTest } from '../../testing/component-test-helper';
+import { assert } from 'node:console';
 
 createComponentTest(DropdownComponent, 'DropdownComponent');
+
+describe('DropdownComponent', ()=> {
+    const { component, fixture } = createComponentTest( DropdownComponent, 'DropdownComponent');
+
+    //TC1: Fisrt state
+    test('should be closed with no selection', ()=> {
+        expect(component.isOpen()).toBe(false);
+        expect(component.selectedOption()).toBe('');
+    })
+
+    
+})
