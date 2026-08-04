74e941cb192d19bc104de39031b87ddc6820b387
LeratoNS <sibandalerato233@gmail.com>
2026-08-04 02:25:44 +0200

    feat: add mock data for testing and imports

--- apps/frontend/client/src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ apps/frontend/client/src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,31 @@
 import { SidebarComponent } from './sidebar.component';
+import { AuthService } from '../../../core/services/auth.service';
 import { createComponentTest } from '../../testing/component-test-helper';
+import { TestBed } from '@angular/core/testing';
+import { signal } from '@angular/core';
+import { Router } from '@angular/router';
 
-createComponentTest(SidebarComponent, 'SidebarComponent');
+// createComponentTest(SidebarComponent, 'SidebarComponent');
+
+describe('SidebarCoponent', () => {
+    let component: SidebarComponent;
+
+    // MOCK authenticated user
+    const currentUser = signal({
+        firstName: 'John',
+        lastName: 'Doe',
+        roles: ['ROLE_ADMIN'],
+    });
+
+    // Mock angular route
+    const routerMock = {
+        navogate: jest.fn(),
+        url: '/dashboard'
+    };
+
+    // Mock authentication service
+    const routeMock = {
+        currentUser,
+        logout: jest.fn(),
+    };
+})
