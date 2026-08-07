65966421859651b7c81c35607676026e92c43bcd
kc-node <kc-node1@gmail.com>
2026-08-07 21:19:40 +0200

    fix: add comments to explain the solution

--- apps/backend/src/main/java/timesheets/service/TokenBlacklistService.java
+++ apps/backend/src/main/java/timesheets/service/TokenBlacklistService.java
@@ -25,6 +25,29 @@ public class TokenBlacklistService {
     redisTemplate.opsForValue().set(BLACKLIST_PREFIX + token, "blacklisted", ttl);
   }
 
+  /*
+    While working on the integration tests for Time Entry feature, 
+    The login request passed and return a valid JWT token 
+    when creating a Time Entry, the request passed through 
+    JwtAuthFilter before getting to the controller,  is authenenticating the user, the tokenBlacklistService.isBlacklisted(token) will verify if the token is blacklisted 
+    then RedisTemplate.hasKey(...) and it tries to connect to Redis since Redis only exist inside the Docker Compose 
+    and at the moment, we dont have 'actual active redis' working 
+
+    why did I get 403?
+    in JwTAuthFilter.java
+    catch (Exception ignored) {
+      // invalid token - just continue unauthenticated
+    }
+
+    nothing gets placed into Security Context Holder so it get to Spring Security as an anonymous user and since 
+    authentication exists, it will return 403
+
+    so solution: for integration tests, we will mock TokenBlackService so that tokenBlacklistService.isBlacklisted(token)  always return false. 
+    This will bypasses Redis, allowing authentication to complete while
+    keeping the integration test focused on the Time Entry functionality rather
+    than Redis infrastructure.
+
+  */
   public boolean isBlacklisted(String token) {
     return Boolean.TRUE.equals(redisTemplate.hasKey(BLACKLIST_PREFIX + token));
   }
