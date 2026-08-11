07b1676b49c1af47f2b05dbd10aeee1fea0b3610
kc-node <kc-node1@gmail.com>
2026-08-11 23:35:56 +0200

    fix: only Service Connection tell Spring Boot about datasource connection from that container

--- apps/backend/src/test/java/timesheets/integration/BaseIntegrationTest.java
+++ apps/backend/src/test/java/timesheets/integration/BaseIntegrationTest.java
@@ -21,8 +21,6 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import timesheets.dto.request.AuthRequest;
 import timesheets.dto.response.AuthResponse;
 
@@ -30,7 +28,6 @@ import timesheets.dto.response.AuthResponse;
     webEnvironment =
         SpringBootTest.WebEnvironment
             .RANDOM_PORT) // assign another random port (NOT 8080 to avoid conflicts)
-@Testcontainers
 @ActiveProfiles("test") // use application-test.properties
 // JUnit create a new object for every method so with PER_CLASS, it creates one object and reuse it
 // in the class
@@ -42,7 +39,7 @@ public abstract class BaseIntegrationTest {
   // creates a PostgreSQL Docker container
   // docker start this container, creates database and spring connects to it
   // and when the tests are done, the container stops and deleted
-  @Container @ServiceConnection
+  @ServiceConnection
   static PostgreSQLContainer<?> postgres =
       new PostgreSQLContainer<>("postgres:15-alpine")
           .withDatabaseName("momently_integration_test")
