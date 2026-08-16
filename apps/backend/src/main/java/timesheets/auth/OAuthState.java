e10556abc942d5d933883e2c15cf72e4f05dc266
kc-node <kc-node1@gmail.com>
2026-08-17 00:13:59 +0200

    create data model

---
+++ apps/backend/src/main/java/timesheets/auth/OAuthState.java
@@ -0,0 +1,18 @@
+package timesheets.integration.auth;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// this is the data object - this what we get after decoding and verifying OAuth state 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OAuthState {
+
+  private UUID workspaceMemberId;
+  private String provider;
+
+}
