db72f3b46df135a60dcc5a5e2b662a6b6960d81d
kc-node <kc-node1@gmail.com>
2026-08-16 14:37:52 +0200

    add: create function to generate stronger state

--- apps/backend/src/main/java/timesheets/auth/GoogleOAuthService.java
+++ apps/backend/src/main/java/timesheets/auth/GoogleOAuthService.java
@@ -9,6 +9,7 @@
 
 package timesheets.integration.auth;
 
+import java.util.UUID; 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,13 @@ public class GoogleOAuthService {
 
   @Value("${app.google.redirect-uri}")
   private String redirectUri;
+
+  // need to generate a stronger and not easy to guess random value 
+  // Cleo need to store in user's session and this will be sent to Google and then after we compare to ensure that the sent UUID matches what Google returns
+
+  public String generateState(){
+    return UUID.randomUUID().toString();
+  }
 
   /* client id identifies our application to Google
       redirect_uri tells google where to send user after authorization
