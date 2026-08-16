3c96d71a0b8507af5b495d3720b842af8ae0a504
kc-node <kc-node1@gmail.com>
2026-08-16 13:54:48 +0200

    fix: rename file, placed files into new folder (calender -> calendar), implement abstract methods with basic return statements

---
+++ apps/backend/src/main/java/timesheets/integration/calendar/google/GoogleCalendarAdapter.java
@@ -0,0 +1,33 @@
+package timesheets.integration.calendar.google;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import timesheets.integration.calendar.CalendarAdapter;
+import timesheets.integration.calendar.CalendarEvent;
+
+public class GoogleCalendarAdapter implements CalendarAdapter {
+  @Override
+  public String getProvider() {
+    // What is specified in dbs
+    return "GOOGLE_CALENDER";
+  }
+
+  @Override
+  public List<CalendarEvent> getEvents(LocalDateTime startTime, LocalDateTime endTime) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public CalendarEvent createEvent(CalendarEvent event) {
+    return event;
+  }
+
+  @Override
+  public CalendarEvent updateEvent(CalendarEvent event) {
+    return event;
+  }
+
+  @Override
+  public void deleteEvent(String externalEventId) {}
+}
