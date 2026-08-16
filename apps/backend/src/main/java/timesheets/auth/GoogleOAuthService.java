c3e2117c61636732b793851a19f30a1e36f6f5fb
kc-node <kc-node1@gmail.com>
2026-08-16 14:10:19 +0200

    add: create url that send users to Google permission screen

--- apps/backend/src/main/java/timesheets/auth/GoogleOAuthService.java
+++ apps/backend/src/main/java/timesheets/auth/GoogleOAuthService.java
@@ -15,6 +15,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class GoogleOAuthService {
 
+  // GOOGLE's primary OAUTH 2.0 endpoint used to authencitate users and request authorization
+  private static final String GOOGLE_AUTHORIZATION_URL =
+      "https://accounts.google.com/o/oauth2/v2/auth";
+
   @Value("${app.google.client-id}")
   private String clientId;
 
@@ -23,4 +27,26 @@ public class GoogleOAuthService {
 
   @Value("${app.google.redirect-uri}")
   private String redirectUri;
+
+  /* client id identifies our application to Google
+      redirect_uri tells google where to send user after authorization
+      response_type - we want google to return the authorization code
+      scope - we want saccess and work with user's calender events
+      state - protects OAuth from CSRF attacks
+      access_type=offline - obtain the refresh token
+  */
+
+ // this is url where the users will be sent to Google Permission screen 
+  public String buildAuthorizationUrl(String state) {
+    return GOOGLE_AUTHORIZATION_URL
+        + "?client_id="
+        + clientId
+        + "&redirect_uri="
+        + redirectUri
+        + "&response_type=code"
+        + "&scope=https://www.googleapis.com/calendar.events"
+        + "&access_type=offline"
+        + "&state="
+        + state;
+  }
 }
