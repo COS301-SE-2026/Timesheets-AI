a5fd9b50b4be2968f748c7c7c77eedee6a6c961f
kc-node <kc-node1@gmail.com>
2026-08-16 15:27:04 +0200

    fix: apply spotless and add import for controller

--- apps/backend/src/main/java/timesheets/controller/GoogleOAuthController.java
+++ apps/backend/src/main/java/timesheets/controller/GoogleOAuthController.java
@@ -1,5 +1,12 @@
 package timesheets.integration.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import timesheets.integration.auth.GoogleOAuthService;
+
+@RestController
+@RequiredArgsConstructor
 public class GoogleOAuthController {
 
+  private final GoogleOAuthService googleOAuthService;
 }
