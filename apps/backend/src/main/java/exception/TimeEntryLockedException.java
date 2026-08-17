acf362c9d7ad3ab1ea8eda3f88bda9d1457a1bf1
unknown <u22783386@tuks.co.za>
2026-08-17 20:54:54 +0200

    feat: removing all the custom exceptions
    - there is actually too many exceptions now, it is getting out of hand. going to use the basic and generic ones now

--- apps/backend/src/main/java/exception/TimeEntryLockedException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-// this will be used when one tries to change a time entry that is locked
-public class TimeEntryLockedException extends StateConflictException {
-
-  public TimeEntryLockedException(String message) {
-    super(message);
-  }
-}
