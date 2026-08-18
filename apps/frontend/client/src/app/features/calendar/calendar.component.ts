8420cefd86b35a8395ae09b5152a4e97f2ab5a14
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-08-18 23:27:52 +0200

    refactor: added the necessary functions i missed for the html divs.

--- apps/frontend/client/src/app/features/calendar/calendar.component.ts
+++ apps/frontend/client/src/app/features/calendar/calendar.component.ts
@@ -1,28 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, viewChild } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject, viewChild, signal, OnInit} from '@angular/core';
 import { FullCalendarComponent, FullCalendarModule} from '@fullcalendar/angular';
-import { CalenderOptions } from '@fullcalendar/core';
+import { CalenderOptions, EventClickArg } from '@fullcalendar/core';
 import { dayGridPlugin} from '@fullcalendar/daygrid';
 import { timeGridPlugin} from '@fullcalendar/timegrid';
-import { CalendarProvider } from './calendar.model';
+import { CalendarProvider, AppEvent } from './calendar.model';
 import { CalendarService } from './calendar.services';
-import { plugins } from 'chart.js';
-
+import { error } from 'node:console';
+export type CalendarView= 'dayGridMonth' | 'timeGridWeek' | 'timeGridDay';
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [ CommonModule, FullCalendarModule],
+  imports: [ CommonModule, FullCalendarModule, DatePipe],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 
-export class CalendarComponent {
-  @viewChild('calendar')calendarComponent!: FullCalendarComponent;
+export class CalendarComponent implements OnInit {
+  @ViewChild('calendar')calendarComponent!: FullCalendarComponent;
 
   private calendarService= inject(CalendarService);
 
-  activeView: CalendarView= 'dayGridMonth';
-  provider: CalendarProvider= 'outlook';
+  activeView= signal<CalendarView>('dayGridMonth');
+  provider= signal<CalendarProvider>( 'outlook');
+  isSyncing= signal<boolean>(false);
+  selectedEvent= signal<AppEvent | null>(null);
 
   calendarOptions: CalenderOptions={
     plugins:[
@@ -35,7 +37,8 @@ export class CalendarComponent {
     height: 'auto',
     editable: false,
     selectable: false,
-    events: []
+    events: [],
+    eventClick: (info: EventClickArg)=> this.handleEventClick(info)
   };
 
   ngOnInit(): void{
@@ -68,8 +71,39 @@ export class CalendarComponent {
     if(direction=== 'today') calander.today();
   }
 
-  changeProvider(provider: CalendarProvider): void{
-    this.provider= provider;
-    this.loadEvents();
+  syncCalendar(): void{
+    this.isSyncing.set(true);
+
+    this.calendarService.getEvents(this.provider(),'', '').subscribe({
+      next: (events)=>{
+        const api= this.calendarComponent?.getApi();
+        if(api){
+          api.removeAllEventSources();
+          api.addEventSource(events);
+        }
+          this.isSyncing.set(false);
+        },
+        error:()=> this.isSyncing.set(false)
+    });
+  }
+
+  handleEventClick(info: EventClickArg): void{
+    const rawProps= info.event.extendedProps;
+
+    this.selectedEvent.set({
+      id: info.event.id,
+      title: info.event.title,
+      start: info.event.start?.toISOString() || '',
+      end: info.event.end?.toISOString() || '',
+      description: rawProps['description'],
+      location: rawProps['location'],
+      provider: this.provider(),
+      category: rawProps['category'],
+      organizer: rawProps['organizer']
+    });
+  }
+
+  closeEventDetails(): void{
+    this.selectedEvent.set(null);
   }
 }
