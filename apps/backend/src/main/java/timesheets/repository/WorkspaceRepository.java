97259d837568baa3f5d07b748109ffceb480b026
unknown <u22783386@tuks.co.za>
2026-08-18 22:27:00 +0200

    feat: adding the workspace respository

---
+++ apps/backend/src/main/java/timesheets/repository/WorkspaceRepository.java
@@ -0,0 +1,26 @@
+package timesheets.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import timesheets.domain.Workspace;
+
+@Repository
+public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
+
+  // Find workspace by name (case insensitive)
+
+  /*
+  - this will find a workspace by the name, and ignore the case
+  - want to prevent workspaces having duplicate names
+  - preventing something like "tech" vs "Tech" */
+  Optional<Workspace> findByNameIgnoreCase(String name);
+
+  //finding the workspaces for a specified user
+  List<Workspace> findByOwnerUserId(UUID ownerUserId);
+
+  //seeing if the workspace with a specific name exists
+  boolean existsByNameIgnoreCase(String name);
+}
