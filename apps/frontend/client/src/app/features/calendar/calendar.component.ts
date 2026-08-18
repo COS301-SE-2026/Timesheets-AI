1131f188605ddd93228205527147104503e1da06
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-08-18 21:39:05 +0200

    feat: add the imports for the calender library.

--- apps/frontend/client/src/app/features/calendar/calendar.component.ts
+++ apps/frontend/client/src/app/features/calendar/calendar.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FullCalendarComponent, FullCalendarModule} from '@fullcalendar/angular';
+import { CalenderOptions } from '@fullcalendar/core';
+import { dayGridPlugin} from '@fullcalendar/daygrid';
+import { timeGridPlugin} from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule, FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
+
 export class CalendarComponent {
 
 }
