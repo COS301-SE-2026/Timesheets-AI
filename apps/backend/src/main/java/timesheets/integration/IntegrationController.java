115dfda0340a8812f3249553e24728840152fb17
kc-node <kc-node1@gmail.com>
2026-08-19 10:51:23 +0200

    add: create callback endpoint

--- apps/backend/src/main/java/timesheets/integration/IntegrationController.java
+++ apps/backend/src/main/java/timesheets/integration/IntegrationController.java
@@ -5,11 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import timesheets.integration.auth.GoogleOAuthService;
+import timesheets.integration.auth.OAuthState;
 import timesheets.integration.auth.OAuthStateService;
 import timesheets.security.SecurityUtils;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/integrations")
@@ -29,18 +30,25 @@ public class IntegrationController {
   }
 
   /* callback endpoint
-  Where: Google sent the result 
+  Where: Google sent the result
    this is when we:
     - receive code (code is temp authorization token)
     - validate state  (what we earlier generated)
-    - exhange code tokens 
-    This is to ensure that the OAuth flow belongs to correct workspace member 
+    - exhange code tokens
+    This is to ensure that the OAuth flow belongs to correct workspace member
   */
 
-    @GetMapping("/google/calender/callback")
-    public ResponseEntity<String> googleCalenderCallBack(@RequestParam String code, @RequestParam String state){
-      // TODO 
-      return ResponseEntity.ok("Activated callback");
-    }
-  
+  /*
+     This prevents hacker from intercept the Google callback and changing
+     the state string to link Google Calendar to different workspaceMemberId
+  */
+  @GetMapping("/google/calendar/callback")
+  public ResponseEntity<String> googleCalendarCallBack(
+      @RequestParam String code, @RequestParam String state) {
+
+    OAuthState validatedState = oauthStateService.validateState(state);
+    return ResponseEntity.ok(
+        "State has been validated for the workspace member: "
+            + validatedState.getWorkspaceMemberId());
+  }
 }
