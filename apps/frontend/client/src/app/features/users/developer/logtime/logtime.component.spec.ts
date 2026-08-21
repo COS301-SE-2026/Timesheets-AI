aea0654cd320161ca4e4b064dc29d99115152b39
LeratoNS <sibandalerato233@gmail.com>
2026-08-21 22:28:45 +0200

    feat: fix failing tests in logtime component

--- apps/frontend/client/src/app/features/users/developer/logtime/logtime.component.spec.ts
+++ apps/frontend/client/src/app/features/users/developer/logtime/logtime.component.spec.ts
@@ -14,6 +14,7 @@ import {
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 // Unit tests for log time page
 
@@ -80,7 +81,7 @@ describe('LogtimeComponent', () => {
     httpMock.expectOne('/api/projects').flush(mockProjects);
    
     //no active timer running at start of normal test, 204 No Content
-    httpMock.expectOne('/api/timers/active').flush(null, {status: 204, statusText: 'No Content'})
+    httpMock.expectOne('/api/timers/active').flush(null, {status: 204, statusText: 'No Content'});
 
     httpMock.expectOne(`/api/tasks/project/${projectOneId}`).flush(
       mockTasks.filter((t)=> t.projectId === projectOneId),);
@@ -89,7 +90,18 @@ describe('LogtimeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LogtimeComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              queryParamMap : {
+                get: jest.fn(),
+              }
+            }
+          }
+        }
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LogtimeComponent);
