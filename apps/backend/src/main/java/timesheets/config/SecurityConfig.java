eba1e79d65ea0016a0094e01b41887da78903e18
unknown <u22783386@tuks.co.za>
2026-08-29 14:47:39 +0200

    fix: fixed the missing import error adder the Value bean

--- apps/backend/src/main/java/timesheets/config/SecurityConfig.java
+++ apps/backend/src/main/java/timesheets/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package timesheets.config;
 
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
