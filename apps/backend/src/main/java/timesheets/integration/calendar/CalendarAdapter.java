222554c9fce776ee97ae1032b5be45ced9d4176e
kc-node <kc-node1@gmail.com>
2026-08-30 23:13:33 +0200

    add workspaceMemberId to identify user

--- apps/backend/src/main/java/timesheets/integration/calendar/CalendarAdapter.java
+++ apps/backend/src/main/java/timesheets/integration/calendar/CalendarAdapter.java
@@ -2,22 +2,24 @@ package timesheets.integration.calendar;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import timesheets.integration.IntegrationAdapter;
 
 public interface CalendarAdapter extends IntegrationAdapter {
   // read
-  List<CalendarEvent> getEvents(LocalDateTime startTime, LocalDateTime endTime);
+  List<CalendarEvent> getEvents(
+      UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime);
 
   // write
   // external provider will generate an ID when event is created
   // Google creates the event and gives us an ID
   // Tha adapter can return title, startTime, endTime and externalEventId
 
-  CalendarEvent createEvent(CalendarEvent event);
+  CalendarEvent createEvent(UUID workspaceMemberId, CalendarEvent event);
 
   // when we update it, the adapter needs to know which external event
-  CalendarEvent updateEvent(CalendarEvent event);
+  CalendarEvent updateEvent(UUID workspaceMemberId, CalendarEvent event);
 
   // delte using eternaleventId
-  void deleteEvent(String externalEventId);
+  void deleteEvent(UUID workspaceMemberId, String externalEventId);
 }
