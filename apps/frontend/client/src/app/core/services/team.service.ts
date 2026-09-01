89f58fb512d0166023f5ae9b4aa7f69944b96128
LeratoNS <sibandalerato233@gmail.com>
2026-09-01 17:36:25 +0200

    feat: define team user and workspace member interfeces

--- apps/frontend/client/src/app/core/services/team.service.ts
+++ apps/frontend/client/src/app/core/services/team.service.ts
@@ -1,3 +1,33 @@
 import { injectable, inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+
+export type WorkspaceRole = 'ADMIN' | 'MANAGER' | 'DEVELOPER';
+
+export interface AvailableTeamUser {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    isInWorkspace: boolean;
+}
+
+export interface WorkspaceMemberResponse {
+    workspaceMemberId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: WorkspaceRole;
+    joinedAt: string;
+}
+
+export interface ProjectMemberResponse {
+    projectMemberId: string;
+    workspaceMemmberId: string;
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    isProjectManager: boolean;
+    joinedAt: string;
+}
