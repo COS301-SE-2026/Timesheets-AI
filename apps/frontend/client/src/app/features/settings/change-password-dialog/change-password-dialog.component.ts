ddddb907e21233a72fcd4f943ee521f88c3da306
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-09-03 17:52:43 +0200

    feat: added the fuctions i require for the pop-up.

--- apps/frontend/client/src/app/features/settings/change-password-dialog/change-password-dialog.component.ts
+++ apps/frontend/client/src/app/features/settings/change-password-dialog/change-password-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
@@ -16,5 +16,51 @@ import { SettingsService } from '../settings.services';
 })
 
 export class ChangePasswordDialogComponent {
+  private settingsService= inject(this.settingsService);
+  private dialogRef= inject(MatDialogRef<ChangePasswordDialogComponent>);
+
+  currentPassword='';
+  newPassword='';
+  confirmPassword='';
+  errorMessage= '';
+  isSubmitting= false;
+
+  get passwordMismatch():boolean{
+    return this.confirmPassword.length>0 && this.newPassword !== this.confirmPassword;
+  }
+
+  submit(): void{
+    this.errorMessage= '';
+
+    if(this.passwordMismatch){
+      this.errorMessage='New password and confirmation do not match.'
+      return;
+    }
+
+    if(this.newPassword.length< 8){
+      this.errorMessage='New password must ne at least 8 characters.'
+      return;
+    }
+
+    this.isSubmitting=true;
+
+    this.settingsService.changePassword(
+      {
+        currentPassword: this.currentPassword,
+        newPassword: this.newPassword,
+        confirmPassword: this.confirmPassword,
+      }
+    ).subscribe(
+      {
+        next: ()=>{
+          this.isSubmitting= false;
+          this.errorMessage= 'Something went wrong. Please try again.'
+        }
+      }
+    );
+  }
+  cancel():void{
+    this.dialogRef.close(false);
+  }
 
 }
