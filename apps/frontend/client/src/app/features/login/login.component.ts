1750bb6626dd514f5c82f78ed52a2a0b6104f34e
unknown <u22783386@tuks.co.za>
2026-09-16 21:47:00 +0200

    fix: using the google default button
    - since the hidden google button was not working on certain pages and unexpected behaviour
    - using that google button only now, since google has a default and microsoft does not, I have to make microsft behave like google

--- apps/frontend/client/src/app/features/login/login.component.ts
+++ apps/frontend/client/src/app/features/login/login.component.ts
@@ -122,7 +122,7 @@ export class LoginComponent implements AfterViewInit {
     google.accounts.id.renderButton(this.googleBtn.nativeElement, {
       theme: 'outline',
       size: 'large',
-      width: 320,
+      width: 210,
     });
   }
   protected triggerGoogleLogin(): void {
