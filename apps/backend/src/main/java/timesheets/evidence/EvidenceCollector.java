3a23b35bb139d9ba9a8d26834c2de0348401af2b
kc-node <kc-node1@gmail.com>
2026-09-18 11:05:44 +0200

    add: create base class to collect evidences from sources

---
+++ apps/backend/src/main/java/timesheets/evidence/EvidenceCollector.java
@@ -0,0 +1,19 @@
+package timesheets.evidence;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface EvidenceCollector {
+
+    /* here is the base class for
+        - GitHubEvidenceCollector - thinks of it as "Turn useful GitHub activity into EvidenceEvents"
+        - JiraEvidenceCollector 
+        - CalendarEvidenceCollector
+
+        Take information from one particular evidence source and turn it into a list of EvidenceEvent objects
+    */
+
+   // Collect the evidence availiable to this developer 
+    List<EvidenceEvent> collect(UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime);
+}
