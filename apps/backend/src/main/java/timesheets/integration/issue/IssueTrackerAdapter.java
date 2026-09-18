b8b896a0d0f2a693de6052cd64c853cc84ed0504
kc-node <kc-node1@gmail.com>
2026-09-18 16:01:11 +0200

    fix: correct to use right class IssueResponse

--- apps/backend/src/main/java/timesheets/integration/issue/IssueTrackerAdapter.java
+++ apps/backend/src/main/java/timesheets/integration/issue/IssueTrackerAdapter.java
@@ -2,14 +2,14 @@ package timesheets.integration.issue;
 
 import java.util.List;
 import java.util.UUID;
-import timesheets.dto.request.CreateJiraIssueRequest;
-import timesheets.dto.response.JiraIssueResponse;
+import timesheets.dto.request.CreateIssueRequest;
+import timesheets.dto.response.IssueResponse;
 
 public interface IssueTrackerAdapter {
 
   String getProvider();
 
-  List<JiraIssueResponse> getIssues(UUID workspaceMemberId);
+  List<IssueResponse> getIssues(UUID workspaceMemberId);
 
   IssueResponse getIssue(UUID workspaceMemberId, String issueKey);
 
