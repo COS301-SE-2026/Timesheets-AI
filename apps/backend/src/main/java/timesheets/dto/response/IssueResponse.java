cf40b6e97ac27506c57f36d2150ab0a707133886
kc-node <kc-node1@gmail.com>
2026-09-18 15:57:54 +0200

    fix: rename the files

---
+++ apps/backend/src/main/java/timesheets/dto/response/IssueResponse.java
@@ -0,0 +1,34 @@
+package timesheets.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueResponse {
+
+  // making this provider-neutral
+  // summary changed to title (more provider-neutral)
+
+  private String key;
+  private String title;
+  private String status;
+  private String issueType;
+
+  private String description;
+  private String priority;
+  private String assigneeEmail;
+  private String assigneeDisplayName;
+  private String projectKey;
+  private String projectName;
+  private String createdAt;
+  private String updatedAt;
+  private String dueDate;
+
+  private UUID localTaskId;
+}
