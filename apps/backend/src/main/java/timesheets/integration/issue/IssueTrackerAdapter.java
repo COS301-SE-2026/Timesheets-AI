4629766bf762c88bd5da5bf64b2aab82c65a3762
kc-node <kc-node1@gmail.com>
2026-09-19 21:52:26 +0200

    add: create method to getting Jira worklogs

--- apps/backend/src/main/java/timesheets/integration/issue/IssueTrackerAdapter.java
+++ apps/backend/src/main/java/timesheets/integration/issue/IssueTrackerAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 import timesheets.dto.request.CreateIssueRequest;
 import timesheets.dto.response.IssueResponse;
+import timesheets.dto.response.WorklogResponse;
 
 public interface IssueTrackerAdapter {
 
@@ -15,8 +16,9 @@ public interface IssueTrackerAdapter {
   IssueResponse getIssue(UUID workspaceMemberId, String issueKey);
 
   // getting issue within time window
-  List<IssueResponse> getIssues(
-      UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime);
+  List<IssueResponse> getIssues(UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime);
+
+  List<WorklogResponse> getWorklogs(UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime);
 
   // the write operations
   IssueResponse createIssue(UUID workspaceMemberId, CreateIssueRequest request);
