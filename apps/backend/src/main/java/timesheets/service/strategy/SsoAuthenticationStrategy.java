5328dd0a1ec356da2a747abdafbbc98cdf7b8a9b
unknown <u22783386@tuks.co.za>
2026-09-19 12:37:35 +0200

    feat: adding the interface class for the strategy design pattern
    - added the strategy, that the concrete strategies will inherit
    - also added the SSO dto

---
+++ apps/backend/src/main/java/timesheets/service/strategy/SsoAuthenticationStrategy.java
@@ -0,0 +1,13 @@
+package timesheets.service.strategy;
+/*
+- this class is the strategy - the interface
+-  each class will inherit from here
+*/
+public interface SsoAuthenticationStrategy {
+
+  //finds the specific provider the strategy handles
+  String getProvider();
+
+  //gets the provider token and extracts a authenticated user
+  SsoUserInfo authenticate(String idToken);
+}
