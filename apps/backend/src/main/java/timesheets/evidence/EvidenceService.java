404807cace519029981e2cb7497170f64e984e09
kc-node <kc-node1@gmail.com>
2026-09-22 13:38:24 +0200

    add: implement to collect from all sources and add to eveidenceEvents,  correct exception name

--- apps/backend/src/main/java/timesheets/evidence/EvidenceService.java
+++ apps/backend/src/main/java/timesheets/evidence/EvidenceService.java
@@ -4,34 +4,39 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Comparator;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service 
+@Service
 @RequiredArgsConstructor
 @Slf4j
 public class EvidenceService {
-    private final List<EvidenceCollector> evidenceCollectors;
+  private final List<EvidenceCollector> evidenceCollectors;
 
-    public List<EvidenceEvent> collect(UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime){
-        List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
+  public List<EvidenceEvent> collect(
+      UUID workspaceMemberId, LocalDateTime startTime, LocalDateTime endTime) {
+    List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
 
-        for ( EvidenceCollector collector : evidenceCollectors){
-            try {
-                List<EvidenceEVent> collectorEvents = collector.collect(workspaceMemberId, startTime, endTime);
+    for (EvidenceCollector collector : evidenceCollectors) {
+      try {
+        List<EvidenceEvent> collectorEvents =
+            collector.collect(workspaceMemberId, startTime, endTime);
 
-                if (collectorEvents !=  null){
-                    evidenceEvents.addAll(collectorEvents);
-                }
-            } catch (Exception e){
-                log.error("Failed to collect evidence using", collector.getClass().getSimpleName(), exception);
-            }
+        if (collectorEvents != null) {
+          evidenceEvents.addAll(collectorEvents);
         }
-
-        evidenceEvents.sort(Comparator.comparing(EvidenceEvent::getTimestamp, Comparator.nullsLast(Comparator.naturalOrder())));
-
-        return evidenceEvents;
+      } catch (Exception e) {
+        log.error("Failed to collect evidence using", collector.getClass().getSimpleName(), e);
+      }
     }
+
+    evidenceEvents.sort(
+        Comparator.comparing(
+            EvidenceEvent::getTimestamp, Comparator.nullsLast(Comparator.naturalOrder())));
+
+    return evidenceEvents;
+  }
 }
