201b3a6041b185eeb3ed8b4ce55386057bf7733a
kc-node <kc-node1@gmail.com>
2026-09-22 12:13:55 +0200

    add: add comments

--- apps/backend/src/main/java/timesheets/evidence/correlation/EvidenceGroup.java
+++ apps/backend/src/main/java/timesheets/evidence/correlation/EvidenceGroup.java
@@ -13,10 +13,10 @@ import timesheets.evidence.EvidenceEvent;
 /* if events will treated related if they occur within 60 minutes window but first we will check the workspaceMemberId*/
 @Data
 public class EvidenceGroup {
-    private UUID id;
-    private UUID workspaceMemberId;
+    private UUID id; // this identifies the particular group - correlated collection of evidence 
+    private UUID workspaceMemberId; // tells us which developer belong to the group 
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
-    private Double correlationScore;
+    private double correlationScore;
 }
