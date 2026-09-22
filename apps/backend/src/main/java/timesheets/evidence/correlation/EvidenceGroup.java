234dbbaac1bd077204f2cae368b5a91aee968aea
kc-node <kc-node1@gmail.com>
2026-09-22 13:39:49 +0200

    add: create EvidenceGroup data model

--- apps/backend/src/main/java/timesheets/evidence/correlation/EvidenceGroup.java
+++ apps/backend/src/main/java/timesheets/evidence/correlation/EvidenceGroup.java
@@ -9,14 +9,13 @@ import java.util.UUID;
 import lombok.Data;
 import timesheets.evidence.EvidenceEvent;
 
-
 /* if events will treated related if they occur within 60 minutes window but first we will check the workspaceMemberId*/
 @Data
 public class EvidenceGroup {
-    private UUID id; // this identifies the particular group - correlated collection of evidence 
-    private UUID workspaceMemberId; // tells us which developer belong to the group 
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
-    private double correlationScore;
+  private UUID id; // this identifies the particular group - correlated collection of evidence
+  private UUID workspaceMemberId; // tells us which developer belong to the group
+  private LocalDateTime startTime;
+  private LocalDateTime endTime;
+  private List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
+  private double correlationScore;
 }
