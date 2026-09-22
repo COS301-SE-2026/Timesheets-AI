d5a26c29342153146972b2b12478140088854eaa
kc-node <kc-node1@gmail.com>
2026-09-22 22:53:28 +0200

    addL SUggested Work Session data model

--- apps/backend/src/main/java/timesheets/suggestions/SuggestedWorkSession.java
+++ apps/backend/src/main/java/timesheets/suggestions/SuggestedWorkSession.java
@@ -2,20 +2,23 @@ package timesheets.suggestions;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Lis.t;
 import java.util.UUID;
 import lombok.Data;
 import timesheets.evidence.EvidenceEvent;
 
 @Data
 public class SuggestedWorkSession {
-    private UUID id;
-    private UUID workspaceMemberId;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private String title; // what we think the developer is using - will use AI to make it more readable
-    private UUID projectId;
-    private UUID taskId; 
-    private List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
-    private double confidenceScore;
+  private UUID id;
+  private UUID workspaceMemberId;
+  private LocalDateTime startTime;
+  private LocalDateTime endTime;
+  // what we think the developer is using - will use AI to make it more readable
+  private String title;
+  private UUID projectId;
+  private UUID taskId;
+  private List<EvidenceEvent> evidenceEvents = new ArrayList<EvidenceEvent>();
+  private double confidenceScore;
+  private Integer durationMinutes;
+  private String explanation;
+  private SuggestionStatus status;
 }
