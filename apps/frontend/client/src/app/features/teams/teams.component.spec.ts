bd4f29d2facee8f12c2a6e163c802d5db0c75cfc
LeratoNS <sibandalerato233@gmail.com>
2026-09-23 20:34:07 +0200

    feat: fix failing tests

--- apps/frontend/client/src/app/features/teams/teams.component.spec.ts
+++ apps/frontend/client/src/app/features/teams/teams.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { TeamsComponent } from './teams.component';
+import { provideRouter } from '@angular/router';
 
 describe('TeamsComponent', () => {
   let component: TeamsComponent;
@@ -9,7 +10,7 @@ describe('TeamsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TeamsComponent],
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient(), provideRouter([])]
     })
     .compileComponents();
 
