9860da2387c051d4943e77e1bbcf3d7fefd4a333
Cleopatra-K <cleopatrakwenda09@gmail.com>
2026-09-24 15:43:07 +0200

    feat: added the files for disable.

--- apps/frontend/client/src/app/features/settings/mfa-setup-dialog/mfa-setup-dialog.component.ts
+++ apps/frontend/client/src/app/features/settings/mfa-setup-dialog/mfa-setup-dialog.component.ts
@@ -12,7 +12,6 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { SettingsService } from "../settings.services";
-import { error } from "node:console";
 
 @Component({
     selector: 'app-mfa-setup-dialog',
